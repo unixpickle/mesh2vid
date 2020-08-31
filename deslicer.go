cfbd694cff8ff7f27609f3f9d9ab0fdd9e190c7d
Alex Nichol <unixpickle@gmail.com>
2020-08-31 17:10:20 -0400

    create vid2mesh decoder

---
+++ deslicer.go
@@ -0,0 +1,83 @@
+package mesh2vid
+
+import (
+	"image"
+	"image/color"
+	"math"
+
+	"github.com/unixpickle/model3d/model2d"
+	"github.com/unixpickle/model3d/model3d"
+)
+
+// A Deslicer is a Solid that interpolates between 2D
+// slices.
+type Deslicer struct {
+	delta       float64
+	zDelta      float64
+	smoothIters int
+
+	layers []model2d.SDF
+	min    model3d.Coord3D
+	max    model3d.Coord3D
+}
+
+func NewDeslicer(delta float64, stride, smoothIters int) *Deslicer {
+	return &Deslicer{
+		delta:       delta,
+		zDelta:      delta * float64(stride),
+		smoothIters: smoothIters,
+
+		min: model3d.Coord3D{},
+		max: model3d.Coord3D{},
+	}
+}
+
+// AddFrame adds a layer to the model on top of the
+// previous layers on the Z axis.
+func (d *Deslicer) AddFrame(frame image.Image) {
+	mesh := model2d.NewBitmapImage(frame, func(c color.Color) bool {
+		r, _, _, _ := c.RGBA()
+		return r < 0xffff/2
+	}).Mesh().SmoothSq(d.smoothIters).Scale(d.delta)
+	sdf := model2d.MeshToSDF(mesh)
+	d.layers = append(d.layers, sdf)
+	d.max.Z = float64(len(d.layers)) * d.zDelta
+
+	min, max := mesh.Min(), mesh.Max()
+	d.max.X = math.Max(d.max.X, max.X)
+	d.max.Y = math.Max(d.max.Y, max.Y)
+	d.min.X = math.Min(d.min.X, min.X)
+	d.min.Y = math.Min(d.min.Y, min.Y)
+}
+
+// Min gets the current minimum coordinate in the solid.
+func (d *Deslicer) Min() model3d.Coord3D {
+	return d.min
+}
+
+// Max gets the current maximum coordinate in the solid.
+func (d *Deslicer) Max() model3d.Coord3D {
+	return d.max
+}
+
+func (d *Deslicer) Contains(c model3d.Coord3D) bool {
+	if !model3d.InBounds(d, c) || len(d.layers) == 0 {
+		return false
+	}
+	z1 := int(c.Z / d.zDelta)
+	z2 := int(c.Z/d.zDelta + 1)
+	s1 := d.getLayerSDF(z1, c)
+	s2 := d.getLayerSDF(z2, c)
+	fracInZ2 := (c.Z - float64(z1)*d.zDelta) / d.zDelta
+	sdf := s1*(1-fracInZ2) + s2*fracInZ2
+	return sdf > 0
+}
+
+func (d *Deslicer) getLayerSDF(layerIdx int, c model3d.Coord3D) float64 {
+	if layerIdx < 0 {
+		layerIdx = 0
+	} else if layerIdx >= len(d.layers) {
+		layerIdx = len(d.layers) - 1
+	}
+	return d.layers[layerIdx].SDF(c.XY())
+}
---
+++ vid2mesh/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/unixpickle/essentials"
+	"github.com/unixpickle/ffmpego"
+	"github.com/unixpickle/mesh2vid"
+	"github.com/unixpickle/model3d/model3d"
+)
+
+func main() {
+	var delta float64
+	var mcDelta float64
+	var zStride int
+	var smoothIters int
+	flag.Float64Var(&delta, "delta", 0.04, "spatial quantization")
+	flag.Float64Var(&mcDelta, "mc-delta", 0,
+		"delta to use for maching cubes, if different than the spatial delta above")
+	flag.IntVar(&zStride, "z-stride", 1, "stride along Z axis (to reduce Z resolution)")
+	flag.IntVar(&smoothIters, "smooth", 20, "smoothing iterations to apply to each frame")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: vid2mesh [flags] <input.mp4> <output.stl>")
+		fmt.Fprintln(os.Stderr)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	flag.Parse()
+	if mcDelta == 0 {
+		mcDelta = delta
+	}
+
+	if len(flag.Args()) != 2 {
+		flag.Usage()
+	}
+	inFile := flag.Args()[0]
+	outFile := flag.Args()[1]
+
+	deslicer := mesh2vid.NewDeslicer(delta, zStride, smoothIters)
+
+	log.Println("Reading slices from video...")
+	vr, err := ffmpego.NewVideoReader(inFile)
+	essentials.Must(err)
+	defer vr.Close()
+	for i := 0; true; i++ {
+		frame, err := vr.ReadFrame()
+		if err == io.EOF {
+			break
+		}
+		essentials.Must(err)
+		log.Printf("Processing frame %d...", i+1)
+		deslicer.AddFrame(frame)
+	}
+
+	log.Println("Converting sliced solid to mesh ...")
+	mesh := model3d.MarchingCubesSearch(deslicer, mcDelta, 8)
+	log.Printf("Saving to %s...", outFile)
+	mesh.SaveGroupedSTL(outFile)
+}
